6384b304cfdf16fcdca85bc487221725348f9281
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-17 13:56:27 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    matches = []
+    possible_anagrams.each do |possible_match|
+      if possible_match.split("").sort == word.split("").sort
+        matches << possible_match
+      end
+    end
+    matches
+  end
+
+end
